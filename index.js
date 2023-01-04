38257c483b3ae3970103d353a6ffe794b383cdbb
Michael Buser <michael.buser@searchdiscovery.com>
2023-01-04 14:34:09 -0500

    linting, added video rule adjustments

--- index.js
+++ index.js
@@ -1,33 +1,50 @@
-const getAdobeInstances = require('./src/connection');
+const getAdobeInstances = require("./src/connection");
 
 const { PROPERTYID } = process.env;
-const delegate_descriptor_id = 'adobe-analytics::actions::send-beacon';
+const delegate_descriptor_id = "adobe-analytics::actions::send-beacon";
 
-async function fetchAllRules(
-  reactor,
-  id,
-  page = 0,
-  previous = [],
-  ) {
-  const data = await reactor.listRulesForProperty(id,{
-      'page[number]': page,
-      'page[size]': 100,
-    }).catch(e => {
+async function fetchAllRules(reactor, id, page = 0, previous = []) {
+  const data = await reactor
+    .listRulesForProperty(id, {
+      "page[number]": page,
+      "page[size]": 100,
+    })
+    .catch((e) => {
       throw new Error(`Error with listFunction: ${e}`);
     });
 
+  const response = [...previous, ...data.data];
 
-    const response = [...previous, ...data.data];
+  if (data?.meta?.pagination?.["next_page"]) {
+    return fetchAll(reactor, id, data?.meta?.pagination["next_page"], response);
+  }
 
-    if (data?.meta?.pagination?.['next_page']) {
-      return fetchAll(reactor, id, data?.meta?.pagination['next_page'], response);
-    }
-
-    return response;
+  return response;
 }
 
-async function fetchAllRuleComponents (reactor, rules = []) {
-  const ruleComponents = await Promise.all(rules.map(async rule => { const r = await reactor.listRuleComponentsForRule(rule?.id); return { ruleName: rule?.attributes?.name, ruleId: rule?.id, ruleComponents: r?.data?.filter(rc => rc?.attributes?.delegate_descriptor_id === delegate_descriptor_id && JSON.parse(rc?.attributes?.settings)?.type === 'link') }}));
+async function fetchAllRuleComponents(reactor, rules = []) {
+  const ruleComponents = await Promise.all(
+    rules.map(async (rule) => {
+      const r = await reactor.listRuleComponentsForRule(rule?.id);
+      const rcs = r?.data?.reduce((acc, rc) => {
+        if (
+          rc?.attributes?.delegate_descriptor_id === delegate_descriptor_id &&
+          JSON.parse(rc?.attributes?.settings)?.type === "link"
+        ) {
+          acc.push({
+            ...rc,
+            ruleName: rule?.attributes?.name,
+          });
+        }
+        return acc;
+      }, []);
+      return {
+        ruleName: rule?.attributes?.name,
+        ruleId: rule?.id,
+        ruleComponents: rcs,
+      };
+    })
+  );
 
   return ruleComponents;
 }
@@ -35,44 +52,62 @@ async function fetchAllRuleComponents (reactor, rules = []) {
 const test = async () => {
   try {
     const { reactor } = await getAdobeInstances();
-   
+
     const rules = await fetchAllRules(reactor, PROPERTYID);
-    
+
     const ruleComponentsGrouped = await fetchAllRuleComponents(reactor, rules);
 
-    const ruleComponents = ruleComponentsGrouped.reduce((acc, ruleComponentGroup) => {
-      if (ruleComponentGroup?.ruleComponents?.length) {
-        acc = [...acc, ...ruleComponentGroup.ruleComponents];
-      }
-      return acc;
-    }, []);
+    const ruleComponents = ruleComponentsGrouped.reduce(
+      (acc, ruleComponentGroup) => {
+        if (ruleComponentGroup?.ruleComponents?.length) {
+          acc = [...acc, ...ruleComponentGroup.ruleComponents];
+        }
+        return acc;
+      },
+      []
+    );
 
     console.log(`Updating ${ruleComponents?.length} ruleComponents`);
 
-    const updatedRuleComponents = await Promise.all(ruleComponents.map(async ruleComponent => {
-      const settings = JSON.parse(ruleComponent?.attributes?.settings);
-      console.log(`Updating ${ruleComponent?.id}: ${settings?.linkName}`);
-      const updatedRuleComponent = await reactor.updateRuleComponent({
-        id: ruleComponent?.id,
-        attributes: {
-          ...ruleComponent?.attributes,
-          settings: JSON.stringify({
-            ...settings,
-            linkName: '%Custom Link Name | [APL]%',
-          }),
-        },
-        type: 'rule_components',
-      });
+    const videoRules = [
+      "Video Started | [AA]",
+      "Video Played 25% | [AA]",
+      "Video Played 50% | [AA]",
+      "Video Played 75% | [AA]",
+      "Video Completed | [AA]",
+      "Video Abandoned | [AA]",
+    ];
 
-      return updatedRuleComponent;
-    })).catch(e => {
+    const updatedRuleComponents = await Promise.all(
+      ruleComponents.map(async ({ ruleName, ...ruleComponent }) => {
+        const settings = JSON.parse(ruleComponent?.attributes?.settings);
+        console.log(
+          `Updating ${ruleComponent?.id} for ${ruleName}. Current Link Name: ${settings?.linkName}`
+        );
+        const updatedRuleComponent = await reactor.updateRuleComponent({
+          id: ruleComponent?.id,
+          attributes: {
+            ...ruleComponent?.attributes,
+            settings: JSON.stringify({
+              ...settings,
+              linkName: videoRules.includes(ruleName)
+                ? "%Video | VideoName | [APL]%"
+                : "%Custom Link Name | [APL]%",
+            }),
+          },
+          type: "rule_components",
+        });
+
+        return updatedRuleComponent;
+      })
+    ).catch((e) => {
       throw new Error(`Error updating ruleComponents: ${e}`);
     });
 
-    console.log('SUCCESS');
+    console.log("SUCCESS");
   } catch (err) {
     console.log(err);
   }
-}
+};
 
 test();
